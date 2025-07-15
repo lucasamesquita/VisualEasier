0a5b6a7a5f841c84f716ea43db75a38ce3d06204
Guilherme Ferreira <43217944+gferrsilva@users.noreply.github.com>
2025-07-15 13:57:11 -0300

    Add files via upload

---
+++ requirements.R
@@ -0,0 +1,7 @@
+
+# Install all required packages
+install.packages(c(
+  "shiny", "shinythemes", "tidyverse", "readr", "reshape2", "raster", 
+  "data.table", "patchwork", "Cairo", "imager", "pals", 
+  "rsconnect", "rasterVis", "ggpubr"
+))
