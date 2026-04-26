43d453ea6e1b7e5a084a2563fd97d3172808cb6c
Guilherme Ferreira <43217944+gferrsilva@users.noreply.github.com>
2026-04-26 10:57:34 -0300

    Modify required packages in requirements.R
    Updated the list of required R packages for the application.

--- app/requirements.R
+++ app/requirements.R
@@ -1,7 +1,8 @@
 
 # Install all required packages
 install.packages(c(
-  "shiny", "shinythemes", "tidyverse", "readr", "reshape2", "raster", 
-  "data.table", "patchwork", "Cairo", "imager", "pals", 
-  "rsconnect", "rasterVis", "ggpubr"
+  "shiny", "shinythemes", "data.table", "dplyr", "tidyr", "stringr",
+  "ggplot2", "patchwork", "Cairo", "imager", "pals", "ggpubr",
+  "shinycssloaders", "scales", "png", "colourpicker", "RColorBrewer",
+  "plotly", "corrplot", "htmlwidgets", "viridisLite", "GGally"
 ))
